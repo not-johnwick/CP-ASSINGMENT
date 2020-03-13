898432199adf98f11dffb39c28eaffb4b2b1e3ef
JohnwickFO <62132447+not-johnwick@users.noreply.github.com>
2020-03-13 17:34:56 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+    {
+     float principle,time,rate,SI;
+     printf("enter principle:");
+     scanf("%f",&priciple);
+     printf("enter time:");
+     scanf("%f",&time);
+     printf("enter rate:");
+     scanf("%f",&rate);
+     SI=(principle*time*rate)/100
+     printf("simple interest=%f",SI);
+    } 
+     
