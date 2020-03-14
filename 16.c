3cb602c70214bcd62db4d2943f40ad2c92e07b19
JohnwickFO <62132447+not-johnwick@users.noreply.github.com>
2020-03-14 12:01:27 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+intmain()
+{
+    int number, i = 1;
+ 
+    printf(" Enter the Number:");
+    scanf("%d", &number);
+    printf("Multiplication table of %d:\n ", number);
+    while (i <= 10)
+    {
+        printf(" %d x %d = %d \n ", number, i, number * i);
+        i++;
+    }
+    return 0;
+}
