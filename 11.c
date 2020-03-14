149c17dfa3d21788af2a4ae33431a50239ed0b6c
JohnwickFO <62132447+not-johnwick@users.noreply.github.com>
2020-03-14 11:45:28 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+voidmain()
+ {
+   int num1, num2, flag_var, i, j;
+ 
+   /* Ask user to input the from/to range
+    * like 1 to 100, 10 to 1000 etc.
+    */
+   printf("Enter two range(input integer numbers only):");
+   scanf("%d %d", &num1, &num2);
+   printf("Prime numbers from %d and %d are:\n", num1, num2);
+   for(i=num1+1; i<num2; ++i)
+   {
+      flag_var=0;
+      for(j=2; j<=i/2; ++j)
+      {
+         if(i%j==0)
+         {
+            flag_var=1;
+            break;
+         }
+      }
+      if(flag_var==0)
+         printf("%d\n",i);
+   }
+ } 
